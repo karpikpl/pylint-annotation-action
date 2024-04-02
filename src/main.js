aabcc7fe4ba156cd8b53685d2fb7ac9fab4f8ff9
Piotr Karpala <piotrkarpala@microsoft.com>
2024-04-02 12:35:56 -0400

    working on tests

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 const core = require('@actions/core')
-const { wait } = require('./wait')
+const github = require('@actions/github')
+const fs = require('fs')
 
 /**
  * The main function for the action.
@@ -7,21 +8,75 @@ const { wait } = require('./wait')
  */
 async function run() {
   try {
-    const ms = core.getInput('milliseconds', { required: true })
+    const lintFile = core.getInput('lint-file', { required: true })
+    const pylintResultCode = +core.getInput('pylint-result-code', {
+      required: true
+    })
+    const headSha = core.getInput('head-sha', { required: true })
+    const token = core.getInput('repo-token')
+    const repo_owner = core.getInput('repo-owner')
+    const repo_name = core.getInput('repo-name')
 
-    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
+    // read summary file
+    const lintingJson = fs.readFileSync(lintFile, 'utf8')
+    const linting = JSON.parse(lintingJson)
 
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const annotations = linting.messages.map(result => {
+      return {
+        path: result.path,
+        start_line: result.line,
+        end_line: result.endLine || result.line,
+        annotation_level: toConvention(result.type),
+        message: result.message,
+        start_column: result.column,
+        end_column: result.endColumn || result.column
+      }
+    })
+    const conclusion = pylintResultCode === 0 ? 'success' : 'failure'
 
-    // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString())
+    core.info(`conclusion of linting: ${conclusion}`)
+
+    const octokit = github.getOctokit(token)
+
+    const resp = await octokit.rest.checks.create({
+      owner: repo_owner,
+      repo: repo_name,
+      name: 'pylint',
+      head_sha: headSha,
+      completed_at: new Date().toISOString(),
+      conclusion,
+      status: 'completed',
+      output: {
+        title:
+          conclusion === 'success'
+            ? 'No issues have been found!'
+            : 'Pylint has some suggestions!',
+        summary:
+          conclusion === 'success'
+            ? 'No issues have been found!'
+            : 'Pylint has some suggestions!',
+        annotations
+      }
+    })
+    core.debug(`response from checks create: ${resp.status}`)
+    core.setOutput('result', 'Success')
   } catch (error) {
     // Fail the workflow run if an error occurs
     core.setFailed(error.message)
+    core.setOutput('result', 'Failure')
+  }
+}
+
+function toConvention(c) {
+  switch (c) {
+    case 'convention':
+      return 'notice'
+    case 'error':
+      return 'failure'
+    case 'warning':
+      return 'warning'
+    default:
+      return 'notice'
   }
 }
 
